68ae70091497726a43ccd46b0a2cfe4801c68400
Chawana Maseka <chawana.maseka@gmail.com>
2022-12-21 18:36:39 -0800

    python rocks

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""A module that defines a square """
+
+
+class Square:
+    """A class that represents a square"""
+
+    def __init__(self, size=0):
+        """Initializing this square class
+        Args:
+            size: represnets the size of the square defined
+        Raises:
+            TypeError: if size is not integer
+            ValueError: if size is less than zero
+        """
+
+
