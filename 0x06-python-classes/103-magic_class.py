3892d0866cae1161285e2e7499b3b7dea0298c32
Chawana Maseka <chawana.maseka@gmail.com>
2022-12-28 09:15:57 -0800

    python rocks

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
-"""writing a docstring"""
+"""Defines a class MagicClass"""
 import math
 
 
 class MagicClass:
-    """ writing another docstring """
-
+    """The class for circle"""
     def __init__(self, radius=0):
-	""" writing another docstring """
+	"""The __init__ method for MagicClass class"""
 	self.__radius = 0
 	if type(radius) is not int and type(radius) is not float:
 	    raise TypeError('radius must be a number')
 	self.__radius = radius
 
     def area(self):
-	"""again with the docstring"""
+	"""Calculaes the area of the circle"""
 	return self.__radius ** 2 * math.pi
 
     def circumference(self):
-	"""such docstring"""
+	"""Calculates the perimeter of the circle"""
 	return 2 * math.pi * self.__radius
